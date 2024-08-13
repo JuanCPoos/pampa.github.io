925a85595ae4eadf51a2c6c44c4bc30ff96677cb
JuanCPoos <juanpoos.e@gmail.com>
2024-08-13 13:33:52 -0300

    Correciones varias. Trabajo finalizado

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* js para el carousel de imagenes */
+const carousel = document.getElementById('carousel');
+const carouselImages = document.querySelectorAll('#carousel img');
+const carouselButtons = document.querySelectorAll('#carousel .carousel-button');
+const carouselButtonLeft = document.querySelector('#carousel .carousel-control-prev');
+const carouselButtonRight = document.querySelector('#carousel .carousel-control-next');
+
+
+
+
+
